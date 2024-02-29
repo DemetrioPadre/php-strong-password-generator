d5ece624b6ed4046f107253f063a8cd665509891
DemetrioPadre <demetriopadre@gmail.com>
2024-02-29 15:07:13 +0100

    proviam un altro metodo

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
-//controllare sempre se il form è stato inviato
+
+
+
+$password_lenght = (int) $_GET['password-lenght'] ?? 0;
+
+
+
+//controllare sempre se il form è vuoto
 $form_sent = !empty($_GET);
-
-if ($form_sent) { //se il form è stato inviato allora
-    $password_lenght = $_GET['password-lenght'];
-    //per essere valido deve contenere i parametri richiesti e li inseriamo tramite funzione
-    if (str_contains($password_lenght, $array_upper) && str_contains($password_lenght, $array_lower) && str_contains($password_lenght, $array_numbers) && str_contains($password_lenght, $array_symboli)) {
-        $password_valid = true; // con validità TRUE
-    } else {
-        $password_valid = false; //altrimenti se non li contiene è false
-    }
-}
+if ($form_sent) {
+    $array_caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=';
+    var_dump($array_caratteri);
+};
 
 
 
 
 
-
-$array_upper = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-$array_lower = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-$array_numbers = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-$array_symboli = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/');
 
 
 ?>
@@ -44,9 +40,9 @@ $array_symboli = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_
         <h1>BENVENUTI NEL GENERATORE DI PASSWORD PIù SICURO AL MONDO</h1>
         <form action="index.php" method="GET" class="row">
             <div class="col-10"><label for="password-lenght" class="form-label">Generiamo la tua password </label>
-                <input type="number" class="form-control" id="password-lenght" name="password-lenght" min="1" max="16">
+                <input type="number" class="form-control" id="password-lenght" name="password-lenght" value="password-lenght" min="1" max="16">
             </div>
-            <div class="col-2"> <button type="submit" class="btn btn-primary mt-4">GENERA</button></div>
+            <div class="col-2"> <button type="submit" class="btn btn-primary mt-4 w-100">GENERA</button></div>
 
         </form>
     </div>
